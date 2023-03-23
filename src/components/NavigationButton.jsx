da94981e4cee9a72ba0f62c9aa509f2035925dc9
Jonnycp <jonathan-caputo@hotmail.com>
2023-03-23 20:26:05 +0100

    Navigation button per swiper

---
+++ src/components/NavigationButton.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { Icon } from "@iconify/react";
+
+const IconButton = styled.button`
+  position: absolute;
+  top: 45%;
+  ${({ left }) => (left ? "left: 0;" : "right: 0;")}
+  z-index: 10;
+  background-color: transparent;
+  outline: none;
+  border: none;
+`;
+
+const NavigationButton = ({ left, action }) => {
+  return (
+    <IconButton left={left} onClick={action}>
+      <Icon
+        icon="material-symbols:arrow-back-ios-new-rounded"
+        width={50}
+        hFlip={!left}
+        color="white"
+      />
+    </IconButton>
+  );
+};
+
+export default NavigationButton;
