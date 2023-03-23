d4cac40543a76a61f2043a415539312a0227a943
Jonnycp <jonathan-caputo@hotmail.com>
2023-03-23 20:04:05 +0100

    Main container con sfondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Theme from "./style/Theme";
+import styled from "styled-components";
 
 import Header from "./components/Header";
+import Phrases from "./components/Phrases";
+
+const Container = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background};
+  display: flex;
+  flex-direction: column;
+`;
 
 const App = () => {
+  const [category, setCategory] = useState("animal");
+
   return (
     <Theme>
-        <Header />
+      <Container>
+        <Header category={category}/>
+        <Phrases />
+      </Container>
     </Theme>
   );
 };
