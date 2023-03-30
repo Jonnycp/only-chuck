ce2178483e81645d44ede716e36e842dcf17e023
Jonnycp <jonathan-caputo@hotmail.com>
2023-03-30 23:14:13 +0200

    Funzione getFrase

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,10 @@
 export const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
-    };
+};
+
+export const getFrase = (category) => {
+    return fetch(`https://api.chucknorris.io/jokes/random${category ? `?category=${category}` : ''}`)
+    .then(res => res.json())
+    .then(data => data.value)
+    .catch(err => console.err(err));
+}
