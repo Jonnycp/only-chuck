e24f8c0cbc051e6d98495eb658e80c9206a02e20
Jonnycp <jonathan-caputo@hotmail.com>
2023-03-30 23:14:48 +0200

    generateNew joke

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Theme from "./style/Theme";
 import styled from "styled-components";
 
 import Header from "./components/Header";
 import Phrases from "./components/Phrases";
+import { getFrase } from "./utils/functions";
 
 const Container = styled.main`
   width: 100%;
@@ -14,16 +15,33 @@ const Container = styled.main`
 `;
 
 const App = () => {
-  const [category, setCategory] = useState("animal");
+  const [category, setCategory] = useState("random");
+  const [frasi, setFrasi] = useState([]);
+
+  useEffect(() => generateNew(category, frasi, setFrasi), []);
 
   return (
     <Theme>
       <Container>
-        <Header category={category}/>
-        <Phrases />
+        <Header category={category} />
+        <Phrases frasi={frasi} generateNew={() => generateNew(category, frasi, setFrasi)} />
       </Container>
     </Theme>
   );
+};
+
+const generateNew = (category, frasi, setFrasi) => {
+    let loading = true;
+    const _category = category === "random" ? undefined : category;
+
+    setFrasi([...frasi, "Loading..."]);
+    getFrase(_category).then((res) => {
+      if (loading) {
+        setFrasi([...frasi, res]);
+      }
+    });
+
+    return () => (loading = false);
 };
 
 export default App;
