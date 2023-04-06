16b41b6f37796b25c6e7524051589f72ba735f81
Jonnycp <jonathan-caputo@hotmail.com>
2023-04-06 16:29:15 +0200

    Nuove funzioni per ruota fortuna
    - generateColors
    - randomColor
    - generateSegments
    - getWinner

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,27 @@
 export const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
-    };
+};
+
+export const generateColors = (length) => {
+    const colors = [];
+    for (let i = 0; i < length; i++) {
+        colors.push(randomColor());
+    }
+    return colors;
+};
+
+export const randomColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export const generateSegments = (obj) => {
+    const segments = [];
+    for (let key in obj) {
+        segments.push(obj[key] + " " + capitalize(key));
+    }
+    return segments;
+}
+
+export const getWinner = (winner) => {
+    return winner.split(" ")[1].toLowerCase();
+}
