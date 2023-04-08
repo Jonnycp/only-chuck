2a4fa90ffa412d6a591104cf172c1fe2cbf9bf13
Jonnycp <jonathan-caputo@hotmail.com>
2023-04-08 13:40:58 +0200

    Rimossa funzione preimpostata per actions

--- src/stories/CategoryButton.stories.jsx
+++ src/stories/CategoryButton.stories.jsx
@@ -1,36 +1,53 @@
-import React from 'react';
+import React from "react";
 
 import CategoryButton from "../components/CategoryButton";
 import category2Symbol from "../utils/category2Symbol";
 
 export default {
   component: CategoryButton,
-  title: 'CategoryButton',
+  title: "CategoryButton",
   argTypes: {
-    selected: { control: { type: "select", options: Object.keys(category2Symbol)} },
+    selected: {
+      name: "selected",
+      control: {
+        type: "select",
+        options: Object.keys(category2Symbol),
+        required: true,
+      },
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'random' },
+      },
+      description: "Categoria Selezionata",
+      defaultValue: "random",
+    },
+    setCategory: {
+      action: "setCategory",
+      description: "Setter per la categoria",
+      table: {
+        type: { summary: 'function' },
+      },
+    },
   },
-}
+};
 
 const Template = (args) => <CategoryButton {...args} />;
 
 export const Big = Template.bind({});
 Big.args = {
-    selected: "random",
-    setCategory: (s) => console.log(s)
+  selected: "random"
 };
 
 export const Small = Template.bind({});
 Small.args = {
-    selected: "random",
-    setCategory: (s) => console.log(s)
+  selected: "random"
 };
 Small.parameters = {
-    viewport: {
-        defaultViewport: "mobile1"
-    }
+  viewport: {
+    defaultViewport: "mobile1",
+  },
 };
 export const Error = Template.bind({});
 Error.args = {
-    selected: "CHUCK",
-    setCategory: (s) => console.log(s)
+  selected: "JONNY"
 };
